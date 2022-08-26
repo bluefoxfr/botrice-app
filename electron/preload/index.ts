6f7e5f2c34d8496c38be6bd17dbbbba0e9c2746d
Florian Grima <florian@gamercraft.com>
2022-08-26 20:34:33 +0200

    export function domReady

--- electron/preload/index.ts
+++ electron/preload/index.ts
@@ -1,4 +1,4 @@
-function domReady(condition: DocumentReadyState[] = ['complete', 'interactive']) {
+export function domReady(condition: DocumentReadyState[] = ['complete', 'interactive']) {
   return new Promise(resolve => {
     if (condition.includes(document.readyState)) {
       resolve(true)
